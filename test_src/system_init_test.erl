b930ce06e22fe65e1ae2cc846f5cbdc9bae1e767
joq62 <joakim.leche@gmail.com>
2021-01-11 23:00:38 +0100

    update

--- test_src/system_init_test.erl
+++ test_src/system_init_test.erl
@@ -31,13 +31,13 @@ start()->
     ?assertEqual(ok,setup()),
     ?debugMsg("stop setup"),
 
-    ?debugMsg("Start single_node"),
-%    ?assertEqual(ok,single_node()),
-    ?debugMsg("stop single_node"),
+ %   ?debugMsg("Start single_node"),
+ %   ?assertEqual(ok,single_node()),
+ %   ?debugMsg("stop single_node"),
     
-    ?debugMsg("Start lock_test_1"),
-%    ?assertEqual(ok,lock_test_1()),
-    ?debugMsg("stop lock_test_1"),
+ %   ?debugMsg("Start lock_test_1"),
+ %   ?assertEqual(ok,lock_test_1()),
+ %   ?debugMsg("stop lock_test_1"),
     
    
       %% End application tests
@@ -55,14 +55,14 @@ start()->
 %% Returns: non
 %% --------------------------------------------------------------------
 
-single_node()->
-    AppSpecs=db_app_spec:read_all(),
+single_node(Dbase0)->
+    AppSpecs=rpc:call(Dbase0,db_app_spec,read_all,[],2000),
     AppSpecsId=[AppId||{AppId,_AppVsn,_Type,_Directives,_AppEnvs,_Services}<-AppSpecs],
 
-    ServiceSpecs=db_service_def:read_all(),  
+    ServiceSpecs=rpc:call(Dbase0,db_service_def,read_all,[],2000),  
     ServicesSpecsId=[SpecId||{SpecId,_ServiceId,_ServiceVsn,_StartCmd,_GitPath}<-ServiceSpecs],
-    PassWd=db_passwd:read_all(),
-    Servers=db_server:read_all(),
+    PassWd=rpc:call(Dbase0,db_passwd,read_all,[],2000),
+    Servers=rpc:call(Dbase0,db_server,read_all,[],2000),
       
     ?assertMatch(["master_100_c1.app_spec",
 		  "dbase_100_c2.app_spec",
@@ -106,8 +106,25 @@ setup()->
     ?assertMatch({ok,SyslogNode},slave:start(HostId,syslog,"-pa log/ebin -setcookie abc")),
     ?assertMatch(ok,rpc:call(SyslogNode,application,start,[syslog],5000)),
     ?assertMatch({pong,SyslogNode,syslog},rpc:call(SyslogNode,syslog,ping,[],1000)),
-   % Start dbase
+    % Start dbase
+    DbaseEnvs=[{git_user,"joq62"},{git_pw,"20Qazxsw20"},{cl_dir,"cluster_config"},
+	       {cl_file,"cluster_info.hrl"},{app_specs_dir,"app_specs"},
+	       {service_specs_dir,"service_specs"},
+	       {dbase_nodes,['cluster_dbase@c0','cluster_dbase@c2','cluster_dbase@c2']}
+	      ],
 
+    InitDbaseNode=list_to_atom("init_dbase@"++HostId),
+    ?assertMatch({ok,InitDbaseNode},slave:start(HostId,init_dbase,"-pa dbase/ebin -setcookie abc")),
+
+    ?assertMatch([ok,ok,ok,ok,ok,ok,ok],[rpc:call(InitDbaseNode,application,set_env,[dbase,Par,Val],5000)||{Par,Val}<-DbaseEnvs]),
+    ?assertMatch(ok,rpc:call(InitDbaseNode,application,start,[dbase],3*5000)),
+    ?assertMatch({pong,InitDbaseNode,dbase},rpc:call(InitDbaseNode,dbase,ping,[],1000)),
+
+    ?assertMatch(ok,single_node(InitDbaseNode)),
+    
+    % Working DBase use to start the cluster
+ 
+    
     
     ok.
 %% --------------------------------------------------------------------
